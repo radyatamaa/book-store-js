9984ffc0e8fbe872e407e14a17ae6a36a1ce492b
radyatamaa <youremail@example.com>
2024-03-30 23:36:31 +0700

    fixed app

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,19 +2,27 @@ import React from 'react';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from '@/contexts/ThemeContext'; // Sesuaikan path jika perlu
 import { CartProvider } from '@/contexts/CartContext';
+import Head from 'next/head'; // Import Head from next/head
 
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider>
-      <CartProvider>
-        <Component {...pageProps} />
-      </CartProvider>
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>Book Store</title> {/* Set the title to "Book Store" */}
+        {/* You can also set other meta tags and links here */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <ThemeProvider>
+        <CartProvider>
+          <Component {...pageProps} />
+        </CartProvider>
+      </ThemeProvider>
+    </>
   );
 };
-
 
 export default MyApp;
